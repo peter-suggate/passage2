aa4efead8f5f8c8db4ac93958a359d5484c0b748
Peter Suggate <peter.suggate@anjuna.io>
2022-06-22 17:32:03 +1200

    Bootstrap a Next.js app

---
+++ fsm-renderer/FsmRenderer.tsx
@@ -0,0 +1,58 @@
+import { useState, useCallback } from "react";
+import ReactFlow, {
+  addEdge,
+  FitViewOptions,
+  applyNodeChanges,
+  applyEdgeChanges,
+  Node,
+  Edge,
+  NodeChange,
+  EdgeChange,
+  Connection,
+} from "react-flow-renderer";
+import styles from "./FsmRenderer.module.css";
+
+const initialNodes: Node[] = [
+  { id: "1", data: { label: "Node 1" }, position: { x: 5, y: 5 } },
+  { id: "2", data: { label: "Node 2" }, position: { x: 5, y: 100 } },
+];
+
+const initialEdges: Edge[] = [{ id: "e1-2", source: "1", target: "2" }];
+
+const fitViewOptions: FitViewOptions = {
+  padding: 0.2,
+};
+
+export const FsmRenderer = () => {
+  const [nodes, setNodes] = useState<Node[]>(initialNodes);
+  const [edges, setEdges] = useState<Edge[]>(initialEdges);
+
+  const onNodesChange = useCallback(
+    (changes: NodeChange[]) =>
+      setNodes((nds) => applyNodeChanges(changes, nds)),
+    [setNodes]
+  );
+  const onEdgesChange = useCallback(
+    (changes: EdgeChange[]) =>
+      setEdges((eds) => applyEdgeChanges(changes, eds)),
+    [setEdges]
+  );
+  const onConnect = useCallback(
+    (connection: Connection) => setEdges((eds) => addEdge(connection, eds)),
+    [setEdges]
+  );
+
+  return (
+    <ReactFlow
+      className={styles.fsmRenderer}
+      style={{ position: "absolute" }}
+      nodes={nodes}
+      edges={edges}
+      onNodesChange={onNodesChange}
+      onEdgesChange={onEdgesChange}
+      onConnect={onConnect}
+      fitView
+      fitViewOptions={fitViewOptions}
+    />
+  );
+};
